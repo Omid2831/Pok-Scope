8e24ef7444d6d471844a766fe81bc97f38bacc2e
Omid <mehrabiomid8282@gmail.com>
2025-07-08 23:36:01 +0200

    feat: adding the logic for WRI

--- Pok-Scope/src/components/PokemonCard.jsx
+++ Pok-Scope/src/components/PokemonCard.jsx
@@ -4,7 +4,16 @@ import TypeFonts from "/src/utils/TypeFonts";
 import TypeColors from "/src/utils/TypeColors";
 import "./PokemonCard.css";
 
-function PokemonCard({ name, img, types, abilities, Stats }) {
+function PokemonCard({
+  name,
+  img,
+  types,
+  abilities,
+  Stats,
+  weaknesses,
+  resistances,
+  immunities,
+}) {
   return (
     <div
       className={`${TypeBackgrounds.cards.white} ${TypeBackgrounds.cards.rounded} ${TypeBackgrounds.cards.shadow} ${TypeBackgrounds.cards.padding} ${TypeBackgrounds.cards.spaceY} text-center`}
@@ -14,6 +23,7 @@ function PokemonCard({ name, img, types, abilities, Stats }) {
       </h2>
       {img && <img src={img} alt={name} className="w-32 h-32 mx-auto" />}
 
+      {/* Type */}
       <div>
         <h3 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>
           Type:
@@ -27,6 +37,7 @@ function PokemonCard({ name, img, types, abilities, Stats }) {
         </div>
       </div>
 
+      {/* Abilities */}
       <div>
         <h3 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>
           Abilities:
@@ -39,14 +50,63 @@ function PokemonCard({ name, img, types, abilities, Stats }) {
           ))}
         </div>
       </div>
+
+      {/* Weaknesses, Resistances, Immunities */}
+      <div>
+        <h3 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>
+          Type Matchups:
+        </h3>
+        <div className={TypeBackgrounds.flexCenter}>
+          <div>
+            <h4 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>Weak To:</h4>
+            {weaknesses.length > 0 ? (
+              weaknesses.map((type) => (
+                <span key={type.name} className={TypeBackgrounds.weaknessBadge}>
+                  {type.name}
+                </span>
+              ))
+            ) : (
+              <span className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>None</span>
+            )}
+          </div>
+          <div>
+            <h4 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>Resists To:</h4>
+            {resistances.length > 0 ? (
+              resistances.map((type) => (
+                <span
+                  key={type.name}
+                  className={TypeBackgrounds.resistanceBadge}
+                >
+                  {type.name}
+                </span>
+              ))
+            ) : (
+              <span className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>None</span>
+            )}
+          </div>
+          <div>
+            <h4 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>Immune To:</h4>
+            {immunities.length > 0 ? (
+              immunities.map((type) => (
+                <span key={type.name} className={TypeBackgrounds.typeBadge}>
+                  {type.name}
+                </span>
+              ))
+            ) : (
+              <span className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>None</span>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* Base Stats */}
       <div>
         <h3 className={`${TypeFonts.subheading} ${TypeColors.text.gray700}`}>
           Base Stats:
         </h3>
-        <div className="flex justify-center mt-4">
+        <div className={`${TypeBackgrounds.statsBadge}`}>
           {Array.isArray(Stats) && Stats.length > 0 && (
-            <PokemonRadarChart stats={Stats}
-             />
+            <PokemonRadarChart stats={Stats} pokemonName={name} />
           )}
         </div>
       </div>
