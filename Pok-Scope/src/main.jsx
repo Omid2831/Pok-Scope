eabd0fd10937110a556afd84cc5e26531c1e2255
Omid <mehrabiomid8282@gmail.com>
2025-07-11 11:52:47 +0200

    edit: adding if statment

--- Pok-Scope/src/main.jsx
+++ Pok-Scope/src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'; 
+import App from './App.jsx';
 import { GlobalStyles } from "./utils/Styles.js";
-// Inject global styles
-const styleElement = document.createElement('style');
-styleElement.innerHTML = GlobalStyles;
-document.head.appendChild(styleElement);
+
+// Inject global styles if not already injected
+if (!document.getElementById('global-styles')) {
+  const styleElement = document.createElement('style');
+  styleElement.id = 'global-styles';
+  styleElement.innerHTML = GlobalStyles;  // GlobalStyles must be a CSS string
+  document.head.appendChild(styleElement);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
