9af83c34e186e112845e9e6d7f59b1ff66164306
Omid <mehrabiomid8282@gmail.com>
2025-07-11 14:11:49 +0200

    feat: add a provider around the app

--- Pok-Scope/src/App.jsx
+++ Pok-Scope/src/App.jsx
@@ -1,11 +1,17 @@
-import PoKScope from "./PokScope";
+import PokScope from "./PokScope";
 import "./App.css";
-
+import ThemeProvider from "./context/ThemeProvider";
+import ToggleMode from "./components/ToggleMode";
 
 function App() {
   return (
-     <PoKScope />
+    <ThemeProvider>
+      <div style={{ position: "absolute", top: 10, right: 10 }}>
+        <ToggleMode />
+      </div>
+      <PokScope />
+    </ThemeProvider>
   );
 }
-
 export default App;
+
